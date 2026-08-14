f2afa48437dccd77f8029db6efe9b93323ab7056
lu-var <luc.vargasp@duocuc.cl>
2026-08-13 23:54:32 -0400

    add: parte kotlin completa

--- laboratorio-java-kotlin/src/main/kotlin/cl/duoc/dsy1105/lab/LaboratorioKotlin.kt
+++ laboratorio-java-kotlin/src/main/kotlin/cl/duoc/dsy1105/lab/LaboratorioKotlin.kt
@@ -2,12 +2,35 @@ package cl.duoc.dsy1105.lab
 
 fun main() {
     // TODO 1: declarar modelo, bateria, modoAhorro y propietario.
+    val modelo = "Galaxy A55"
+    val porcentaje = 18
+    val modoBateria = false
+
+
     // TODO 2: mostrar los datos del telefono usando String templates.
+    println("=== TELEFONO ===")
+    println("Modelo: $modelo")
+    println("Bateria: $porcentaje")
+
     // TODO 3: llamar a obtenerEstadoBateria y mostrar el resultado.
+    println("Estado: " + obtenerEstadoBateria(porcentaje))
+    println("Ahorro de energia: $modoBateria")
+
     // TODO 4: usar String? y ?: para el propietario.
+    var nombreProp: String? = null
+    if (nombreProp == null) {
+        nombreProp = "Sin propietario"
+    }
+    println("Propietario: $nombreProp")
 }
 
 fun obtenerEstadoBateria(bateria: Int): String {
     // TODO: implementar las tres reglas indicadas en el README.
-    return ""
+    return if (bateria <= 20) {
+        "Bateria baja"
+    } else if (bateria <= 79) {
+        "Bateria suficiente"
+    } else {
+        "Bateria alta"
+    }
 }
